509312ffee1bfe83d600ecbafd8d45428e0f0647
Dionicio <dioniciomatamacias@gmail.com>
2022-10-04 22:14:15 -0500

    model gods updated

--- models/gods.js
+++ models/gods.js
@@ -2,12 +2,13 @@ const {Sequelize, DataTypes} =require('sequelize');
 const sequelize  = require('../config/db');
 
 const God = sequelize.define('God', {
+    symbol: {
+        type: DataTypes.CHAR(64)
+    },
     name:{
-        symbol: {
-            type: DataTypes.STRING(64)
-        },
-            type: DataTypes.STRING(64)
-    }
+        type: DataTypes.CHAR(64)
+    },
+    
 });
 
 module.exports = God;
