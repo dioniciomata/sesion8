bcb939c6091cd61fea8c41e79d45662aaf142dfe
Dionicio <dioniciomatamacias@gmail.com>
2022-10-04 22:12:05 -0500

    model gods updated

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(
     process.env['DATABASE_URL'],
     
-    {dialectOptions: {
+    {dialect: "postgres",
+    dialectOptions: {
       ssl: {
         require: true, // This will help you. But you will see nwe error
         rejectUnauthorized: false // This line will fix new error
--- models/gods.js
+++ models/gods.js
@@ -3,10 +3,10 @@ const sequelize  = require('../config/db');
 
 const God = sequelize.define('God', {
     name:{
-        type: DataTypes.CHAR(64)
-    },
-    symbol: {
-        type: DataTypes.CHAR(64)
+        symbol: {
+            type: DataTypes.STRING(64)
+        },
+            type: DataTypes.STRING(64)
     }
 });
 
