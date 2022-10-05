f6ee17acbd196a392be1d38834414658be08420b
Dionicio <dioniciomatamacias@gmail.com>
2022-10-04 21:42:14 -0500

    add config of ssl for postgress

--- config/db.js
+++ config/db.js
@@ -1,5 +1,12 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env['DATABASE_URL']);
+const sequelize = new Sequelize(
+    process.env['DATABASE_URL'],
+    
+    {dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }}});
 
 module.exports = sequelize;
