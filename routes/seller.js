744541dc74136f40d4590a8956adb39fcb7e2bd0
Dionicio <dioniciomatamacias@gmail.com>
2022-10-08 20:28:52 -0500

    /seller auth opcional

--- routes/seller.js
+++ routes/seller.js
@@ -7,7 +7,7 @@ const {
 } = require('../controllers/seller');
 const auth = require('../config/auth')
 
-router.get('/', auth.isSeller, getSeller);
+router.get('/', auth.opcional, getSeller);
 router.post('/signup', auth.opcional, signSeller);
 router.post('/login', auth.opcional, logSeller);
 router.delete('/:id', auth.isSeller, deleteSeller);
